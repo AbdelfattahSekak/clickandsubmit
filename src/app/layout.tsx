a53dca480bf50226a64ddd04c23696559b005f00
automated-job@accor-software-factory.com <Automated transfer>
2024-03-23 14:05:33 +0100

    Add Mantine components and styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
+import "@mantine/core/styles.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ColorSchemeScript, MantineProvider } from "@mantine/core";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+        <ColorSchemeScript />
+      </head>
+      <body className="flex min-h-screen">
+        <MantineProvider>{children}</MantineProvider>
+      </body>
     </html>
   );
 }
